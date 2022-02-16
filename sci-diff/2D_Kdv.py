b94277d0e85995b37334df1ec5702f12efb1cc78
Joanna Katarzyna Klara <sc19jkk@leeds.ac.uk>
2022-02-16 14:52:09 +0000

    Changes in 2D KdV - lack of definition?

---
+++ sci-diff/2D_Kdv.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pylab as pl
+import skfdiff as sf
+from skfdiff import Model, Simulation
+from scipy.ndimage import gaussian_filter
+import math
+
+shallow_2D = sf.Model("-U * dxU - e(dxxxU + dxxxU/dyyU)", #write function here appropriately - is the last part good thou?
+                      "U(x, y)", ["e"])
+
+x = np.linspace(-2, 6, 1000)
+y = np.linspace(2, 6, 1000)
+
+n = 20
+U = np.log(1 + np.cosh(n) ** 2 / np.cosh(n * x) ** 2) / (2 * n) #change needed here
+
+initial_fields = shallow_2D.fields_template(x=x, y=y, U=U, e=1)
+
+simulation = Simulation(shallow_2D, initial_fields, dt=0.05, tmax=10)
+container = simulation.attach_container()
+
+simulation.run()
+(
+    container.data.U[: -2 : container.data.t.size // 6].plot(
+        col="t", col_wrap=3, color="black"
+    )
+)
+pl.show()

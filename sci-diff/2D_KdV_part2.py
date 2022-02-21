0295d3727aac1f6e0074c16efbfa65fcbc8c6735
Joanna Katarzyna Klara <sc19jkk@leeds.ac.uk>
2022-02-21 15:37:02 +0000

    Modelling changes, different attack angle on KdV

--- sci-diff/2D_KdV_part2.py
+++ sci-diff/2D_KdV_part2.py
@@ -5,8 +5,13 @@ from skfdiff import Model, Simulation
 from scipy.ndimage import gaussian_filter
 import math
 
-shallow_2D = sf.Model("-U * dxU - e*(dxxxU + dxxxU-dyyU)", #write function here appropriately - is the last part good thou?
-                      "U(x, y)", ["e"])
+shallow_2D = sf.Model(
+    [
+        "-dxU - 3*dxU*dyU - dxxxU",
+        "-dx * U + dy * V"
+     ], #write function here appropriately - is the last part good thou?
+        ["U(x, y)", "V(x, y)"], ["e"]
+)
 
 x = np.linspace(-2, 6, 1000)
 y = np.linspace(2, 6, 1000)
@@ -16,7 +21,7 @@ U = np.log(1 + np.cosh(n) ** 2 / np.cosh(n * x) ** 2 - np.cosh(n * y) ** 2) / (2
 
 initial_fields = shallow_2D.fields_template(x=x, y=y, U=U, e=2e-4)
 
-simulation = Simulation(shallow_2D, initial_fields, dt=0.05, tmax=10)
+simulation = Simulation(shallow_2D, initial_fields, dt=0.05, tmax=5) #tmax=10
 container = simulation.attach_container()
 
 simulation.run()
